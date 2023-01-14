e59ed5fb877a7b9c50259157cbbd90b16a4714b5
Zane <114646680+zanematero@users.noreply.github.com>
2023-01-14 13:10:31 -0800

    Delete index.js

--- Interactive-Map/index.js
+++
@@ -1,9 +0,0 @@
-var fourSquare = fetch("fsq3JjfdCjlBePidRx17mv1EHC/JK/+Yb/iUEem+GsoU4og=", {
-    method: 'GET',
-    headers: {
-        accept: 'application/json'
-    }
-});
-    fourSquare.then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
